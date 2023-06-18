441c5e662a164dcfdb2c5581a283424cdd29819b
Eluk Samuel Kiira <87136417+Eluk-Samuel-Kiira@users.noreply.github.com>
2023-06-18 06:54:28 -0700

    Update history.blade.php

--- resources/views/devices/history.blade.php
+++ resources/views/devices/history.blade.php
@@ -1,7 +1,10 @@
 @extends('dashboard.layout')
 @section('title','Location | History')
-@section('content')
-
+@section('styles')
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+crossorigin=""/>
+@endsection
 @push('scripts')
 <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
 integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
@@ -9,93 +12,91 @@ crossorigin=""></script>
 <!-- Snake animation plugin -->
 <script src="{{asset('assets/js/L.Polyline.SnakeAnim.js')}}"></script>
 @endpush
-
-@section('styles')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-crossorigin=""/>
-@endsection
-
+@section('content')
 <div class="pcoded-inner-content">
     <div class="main-body">
         <div class="page-wrapper">
             <!-- Page body start -->
             <div class="page-body">
                 <div class="row">
-                    @forelse($devices as $device)
+                @forelse($devices as $device)
                         @php
                             $filePath = storage_path('app/public/TripHistories/'.$device->user.'/'.$device->id.'/');
                             $files = glob($filePath . '*.txt');
-                            foreach ($files as $file) {
+                        @endphp    
+                        @foreach($files as $file)
+                            @php
                                 $fileName = basename($file);
                                 $fileNameWithoutExt = pathinfo($fileName, PATHINFO_FILENAME);
-                        @endphp    
-                                <div class="col-lg-12 col-xl-4">
-                                    <!-- Basic map start -->
-                                    <div class="card">
-                                        <div class="card-block">
-                                            <div id="map-{{ $device->id }}-{{ $fileNameWithoutExt }}" class="set-map"></div>
-                                        </div>
-                                        <div class="card-header">
-                                            <h5>Trip History
-                                                <!-- <a href="{{ route('download', ['filename' => $filePath]) }}">Download File</a> -->
-                                            </h5>
-                                            <span>for {{ $device->name }} as of {{ $device->fileNameWithoutExt }} on
-                                                @php
-                                                    $day = \Carbon\Carbon::parse($fileNameWithoutExt)->format('l');
-                                                @endphp
-                                                {{ $day }}
-                                            </span>
-                                        </div>
+                            @endphp
+                            <div class="col-lg-12 col-xl-4">
+                                <!-- Basic map start -->
+                                <div class="card">
+                                    <div class="card-block">
+                                        <!-- Add a div with a unique ID for each map -->
+                                        <div id="map-{{ $device->id }}-{{ $fileNameWithoutExt }}" class="set-map"
+                                             style="height: 400px;"></div>
                                     </div>
-                                    <!-- Basic map end -->
+                                    <div class="card-header">
+                                        <h5>Trip History
+                                            <a href="{{ asset('storage/TripHistories/'.$device->user.'/'.$device->id.'/'.$fileName) }}" download>Download File</a>
+                                        </h5>
+                                    </div>
+                                    <div>
+                                        <span>
+                                            for {{ $device->name }} as of {{ $fileNameWithoutExt }} on
+                                            @php
+                                                $day = \Carbon\Carbon::parse($fileNameWithoutExt)->format('l');
+                                            @endphp
+                                            {{ $day }}
+                                        </span>
+                                    </div>
                                 </div>
-                                @push('scripts')
+                                <!-- Basic map end -->
+                            </div>
+                            @push('scripts')
                                 <script>
-                                    // Use a unique map ID based on device ID and file name
-                                    var mapId = "map-{{ $device->id }}-{{ $fileNameWithoutExt }}";
+                                    $(document).ready(function() {
+                                    // Loop through each map container
+                                    $('.set-map').each(function() {
+                                        // Get the ID of the device associated with this map container
+                                        var mapId = "map-{{ $device->id }}-{{ $fileNameWithoutExt }}";
 
-                                    // Initialize a new map
-                                    var map = L.map(mapId).setView([0.37734, 32.6258], 10);
-                                    // Add a tile layer to the map
-                                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-                                        maxZoom: 18,
-                                    }).addTo(map);
+                                        // Set up the Leaflet map for this device
+                                        var map = L.map(mapId).setView([0.37734, 32.6258], 8);
+                                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                            attribution: '&copy; OpenStreetMap contributors'
+                                        }).addTo(map);
 
-                                    fileContents = @json(file_get_contents($file));
-                                    lines = fileContents.split('\n');
+                                        let fileContents = @json(file_get_contents($file));
+                                        let lines = fileContents.split('\n');
 
-                                    var latlngs = [];
-                                    for (var i = 0; i < lines.length; i++) {
-                                        var line = lines[i].trim();
-                                        if (line !== '') { //skip empty lines
-                                            var data = JSON.parse(line);
-                                            if (data) {
-                                                for (var j = 0; j < data.length; j++) {
-                                                    var item = data[j];
-                                                    var latitude = item.latitude;
-                                                    var longitude = item.longitude;
-                                                    var device_id = item.device_id;
-                                                    // Check if the current array exists in latlngs
-                                                    if (!latlngs.some(function(arr) { return arr[0] === latitude && arr[1] === longitude; })) {
-                                                        var marker = L.marker([latitude, longitude]).addTo(map);
-                                                        latlngs.push([latitude, longitude]);
+                                        var latlngs = [];
+                                        for (let line of lines) {
+                                            if (line.trim() !== '') { //eliminate empty lines
+                                                let data = JSON.parse(line);
+                                                if (data) {
+                                                    for (let item of data) {
+                                                        let latitude = item.latitude;
+                                                        let longitude = item.longitude;
+                                                        let device_id = item.device_id;
+                                                        // Check if the current array exists in latlngs
+                                                        if (!latlngs.some(arr => arr[0] === latitude && arr[1] === longitude)) {
+                                                            var marker = L.marker([latitude, longitude]).addTo(map);
+                                                            latlngs.push([latitude, longitude]);
+                                                        }
                                                     }
                                                 }
                                             }
                                         }
-                                    }
-                                    //set view to the latest coordinates on the file
-                                    map.setView([latitude, longitude], 8);
-                                    var roadLine = L.polyline(latlngs, { color: 'green', snakingSpeed: 200 }).addTo(map);
-                                    roadLine.snakeIn();
-                            
+                                        var roadLine = L.polyline(latlngs, {color: 'green', snakingSpeed: 200}).addTo(map);
+                                        roadLine.snakeIn();
+                                        //console.log(latlngs)
+                                    });
+                                });
                                 </script>
-                        @endpush
-                        @php
-                            }
-                        @endphp    
+                            @endpush
+                        @endforeach
                     @empty
                         <div class="alert alert-Danger">
                             {{ __('No Device History Available') }}
